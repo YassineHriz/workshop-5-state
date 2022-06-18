d167fdb7f27c6a319430653b7556fec90b3e8059
Yassine <yassinehriw@gmail.com>
2022-06-18 16:57:33 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner'
+class App extends React.Component {
+  
+  state={
+    fullName:"Amir",
+    profession:"Singer",
+    bio:"Cool Guy",
+    imgSrc:"https://images.omerlocdn.com/resize?url=https%3A%2F%2Fgcm.omerlocdn.com%2Fproduction%2Fglobal%2Ffiles%2Fimage%2Fa12e6e85-200c-4dd3-a8a2-525e8bd74c23.png&width=1024&type=jpeg&stripmeta=true",
+    display:false
+  };
+  toggleBtn = () =>
+  {
+    this.state.display ? this.setState({display:false}) : this.setState({display:true})
+  }
+  render() {
+    return(
+      <div className="App">
+        <Button variant="success" onClick={this.toggleBtn}>Click Here</Button>
+        {this.state.display ? <div className="Profile"> 
+          <div className='info'>
+            <h1>{this.state.fullName}</h1>
+            <h3>{this.state.profession}</h3>
+            <p>{this.state.bio}</p>
+          </div>
+          <img src={this.state.imgSrc} width="200px" height="auto" alt=""></img>
+        </div> : <Spinner animation="border" variant="secondary" className='Spinner'/>}
+      </div>
+    )
+  }
 }
 
 export default App;
